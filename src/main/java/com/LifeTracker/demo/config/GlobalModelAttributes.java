bf00a13503dd990151e107d5488d0dbdd2760f7e
Yanina Morales <x23437812@student.ncirl.ie>
2025-07-28 22:19:13 +0100

    Thymeleaf initial configuration

---
+++ src/main/java/com/LifeTracker/demo/config/GlobalModelAttributes.java
@@ -0,0 +1,19 @@
+package com.LifeTracker.demo.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalModelAttributes {
+
+    @ModelAttribute("username")
+    public String populateUsername() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.isAuthenticated() && !"anonymousUser".equals(auth.getName())) {
+            return auth.getName();
+        }
+        return null; //when user is not authenticated
+    }
+}
---
+++ src/main/java/com/LifeTracker/demo/controller/ViewController.java
@@ -0,0 +1,19 @@
+package com.LifeTracker.demo.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
+
+
+@Controller
+public class ViewController {
+    @GetMapping("/")
+    public String home(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    String username = auth.getName(); 
+    model.addAttribute("username", username);
+    return "home";
+    }
+}
